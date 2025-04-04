9e1bda439f497c9bec8dc7e94e2e8327d56f3502
Terry <master_correo144@hotmail.com>
2025-04-03 20:53:04 -0500

    ADD search

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+export default async function Page() {
+  await new Promise((res) => {
+    setTimeout(res, 3000, 'Success');
+  });
   return <p>Customers Page</p>;
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,17 @@
 import '@/app/ui/global.css';
 import { inter } from './ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Learn Next',
+    default: 'Learn Next',
+  },
+  icons: {
+    icon: '/learning.svg',
+    shortcut: '/learning.svg',
+  },
+};
 
 export default function RootLayout({
   children,
