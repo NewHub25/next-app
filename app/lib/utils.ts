f8326b6ad848e65662e870949e88d819832af367
Terry <master_correo144@hotmail.com>
2025-04-09 17:33:50 -0500

    CREATE route create

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -42,16 +42,10 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
     return Array.from({ length: totalPages }, (_, i) => i + 1);
   }
 
-  // If the current page is among the first 3 pages,
-  // show the first 3, an ellipsis, and the last 2 pages.
-  if (currentPage <= 3) {
-    return [1, 2, 3, '...', totalPages - 1, totalPages];
-  }
-
-  // If the current page is among the last 3 pages,
-  // show the first 2, an ellipsis, and the last 3 pages.
-  if (currentPage >= totalPages - 2) {
-    return [1, 2, '...', totalPages - 2, totalPages - 1, totalPages];
+  // If the current page is among the first or the last 3 pages,
+  // Show the first 3, an ellipsis, and the last 3 pages.
+  if (currentPage <= 3 || currentPage >= totalPages - 2) {
+    return [1, 2, 3, '...', totalPages - 2, totalPages - 1, totalPages];
   }
 
   // If the current page is somewhere in the middle,
